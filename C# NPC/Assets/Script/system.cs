666954e3e14c722884ff95fa20f83c478297cccb
kami2212 <110316254+kami2212@users.noreply.github.com>
2022-08-26 01:41:44 +0800

    Update system.cs

--- C# NPC/Assets/Script/system.cs
+++ C# NPC/Assets/Script/system.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 namespace Su
@@ -10,16 +9,17 @@ namespace Su
     public class system : MonoBehaviour
     {
         Vector3 offset; 
-        Vector3 offset2;
+        
         bool isDragging;
-        public BoxCollider A3;
+        
+
         private void Update()
         {
 
             if (isDragging)
             {
                 transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - offset;
-                print($"{A3.ClosestPoint(transform.position)}");
+                
             }
             
 
